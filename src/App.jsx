9f1581b364b4f6fd3fa6ec18f08d3fb0031f95fe
quartzs2 <csh001231@gmail.com>
2025-04-10 23:45:00 +0900

    feat: 페이지 컴포넌트로 분리, Not Found 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { Route, Routes } from 'react-router';
 import Layout from './components/Layout';
+import Main from './pages/Main';
+import About from './pages/About';
+import ContactUs from './pages/ContactUs';
+import Favorites from './pages/Favorites';
+import UserInfo from './pages/UserInfo';
+import Blog from './pages/Blog';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<div>mainpage</div>} />
-        <Route path='/about' element={<div>about</div>} />
-        <Route path='/contact-us' element={<div>ContactUs</div>} />
-        <Route path='/blog' element={<div>blog</div>} />
-        <Route path='/favorites' element={<div>favorites</div>} />
-        <Route path='/cart' element={<div>cart</div>} />
-        <Route path='/user-info' element={<div>user-info</div>} />
+        <Route index element={<Main />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact-us' element={<ContactUs />} />
+        <Route path='blog' element={<Blog />} />
+        <Route path='favorites' element={<Favorites />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='user-info' element={<UserInfo />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
   );
