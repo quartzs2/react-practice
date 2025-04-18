3347baa9d96361802c8e4f4812a11c5c1af01781
quartzs2 <csh001231@gmail.com>
2025-04-18 15:29:40 +0900

    feat: 메타데이터 추가(#30)
    - 메타데이터 추가
    - products 페이지 추가
    - main 페이지와 products 페이지에 메타데이터 추가
    - public 폴더에 og_main_page.png, og_product_page.png 추가
    - constants/url.js에 DEFAULT_META_DATA.url 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,17 @@
+import Metadata from '@/components/Metadata';
+import { DEFAULT_META_DATA_URL } from '@/constants/url';
 import Banner from '@components/ui/banner/Banner';
 
 const Main = () => {
   return (
     <div>
+      <Metadata
+        title='cyber 메인 페이지'
+        url={DEFAULT_META_DATA_URL}
+        image={`${DEFAULT_META_DATA_URL}/og_main_page.png`}
+        imageAlt='cyber 메인 페이지 이미지입니다. '
+        description='cyber의 메인 페이지입니다.'
+      />
       <Banner />
     </div>
   );
