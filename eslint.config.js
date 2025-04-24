d38057b11d7f8c623c3bb6926fe7a334c28e93aa
quartzs2 <csh001231@gmail.com>
2025-04-25 01:40:54 +0900

    feat: eslint-plugin-react 추가(#44)
    - eslint-plugin-react 추가
    - button-has-type 규칙 추가
    - eslint.config.js에 react version detect 추가

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,19 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
 
 export default [
   jsxA11y.flatConfigs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
@@ -22,15 +32,16 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react,
     },
     rules: {
       ...js.configs.recommended.rules,
-      // ...js.configs.all.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'prefer-const': 'error',
       'no-var': 'error',
+      'react/button-has-type': 'error',
     },
   },
   eslintConfigPrettier,
